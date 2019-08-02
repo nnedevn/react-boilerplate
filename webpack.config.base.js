dbc2faa7c05e4987c8dd8995a2f1f1e8e080e027
Nick <nick@nicknedev.com>
2019-08-01 21:32:17 -0700

    Updates react-hot-loader config

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -24,7 +24,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-        exclude: /node_modules/ 
+        exclude: /node_modules/
       }
     ]
   },
